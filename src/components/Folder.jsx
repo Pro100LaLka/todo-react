b71e6ada82645351de62e691b32e901654c64faf
Daniil Zhigailov <danil.zhigailov@mail.ru>
2026-09-24 01:29:56 +0300

    fix folder name overflow bug on desktop

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -14,12 +14,12 @@ function Folder({ folder, selectedFolder, onClick, onRemove }) {
       <label
         htmlFor={folder.name}
         className={clsx(
-          "font-inter relative flex cursor-pointer items-center rounded-full bg-slate-900 px-4 py-2 text-base whitespace-nowrap text-neutral-400 transition-[padding] duration-200 select-none group-has-checked:cursor-default group-has-checked:bg-emerald-300 group-has-checked:text-black peer-focus-visible:outline-2 peer-focus-visible:outline-offset-2 peer-focus-visible:outline-gray-200 md:rounded-xl md:bg-transparent md:px-3 md:text-lg md:font-medium md:text-gray-300 md:group-has-checked:bg-emerald-300/20 md:group-has-checked:text-emerald-300 md:hover:bg-gray-800 md:group-has-checked:hover:bg-emerald-300/20",
+          "font-inter relative flex cursor-pointer items-center rounded-full bg-slate-900 px-4 py-2 text-base whitespace-nowrap text-neutral-400 transition-[padding] duration-200 select-none group-has-checked:cursor-default group-has-checked:bg-emerald-300 group-has-checked:text-black peer-focus-visible:outline-2 peer-focus-visible:outline-offset-2 peer-focus-visible:outline-gray-200 md:rounded-xl md:bg-transparent md:px-3 md:text-lg md:font-medium md:whitespace-normal md:text-gray-300 md:group-has-checked:bg-emerald-300/20 md:group-has-checked:text-emerald-300 md:hover:bg-gray-800 md:group-has-checked:hover:bg-emerald-300/20",
           folder.removable && "group-has-checked:pr-8 hover:pr-8",
         )}
       >
-        <span className="mr-4 hidden size-2.5 rounded-full bg-gray-500 group-has-checked:bg-emerald-300 md:inline-block"></span>
-        {folder.name}
+        <span className="mr-4 hidden size-2.5 shrink-0 rounded-full bg-gray-500 group-has-checked:bg-emerald-300 md:inline-block"></span>
+        <span className="md:max-w-48">{folder.name}</span>
         <button
           onClick={() => onRemove(folder.name)}
           className={clsx(
